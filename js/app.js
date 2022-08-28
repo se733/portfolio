4541fcdea86dc451792da46e614502d6e3022402
se733 <chavezsebas737@gmail.com>
2022-08-27 22:57:37 -0300

    commit portfolio

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// 'use strict'
+
+// // $(document).on({ 'ready' : () =>{
+
+
+
+
+// // } })
+
+// $(document).ready(function(){
+
+//     $('body').on({ 'mousemove' : function(e){
+//         console.clear();
+        
+//         let clientX = e.originalEvent.clientX
+//         let clientY = e.originalEvent.clientY
+
+//         $('#cursor').css({
+//             'left' : clientX + 'px',
+//             'top' : clientY + 'px'
+//         })
+//     }})
+
+// })
